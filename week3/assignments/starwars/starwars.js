bfb19b003685a51ed1c89364ad1722aeb3670a0d
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-10-14 00:41:05 -0500

    Complete 'Star Wars Data Checker' assignment

--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -3,6 +3,11 @@
 // define a function called 'fetchData()' that passes the values from 
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
+function fetchData() {
+    queryType = document.querySelector("#queryType").value;
+    itemID = document.querySelector("#itemID").value;
+    console.log(getFromSWAPI());
+}
 
 function getFromSWAPI() {
     // assign values to any necessary variables
@@ -16,6 +21,30 @@ function getFromSWAPI() {
     .catch(function(err) {
         console.warn(err)
     })
+}
+
+function updateInfo(data) {
+    if (queryType == "people")
+    {
+        document.querySelector("#dataLabel1").textContent = "Name: ";
+        document.querySelector("#dataLabel2").textContent = "Hair Color: "
+        document.querySelector("#dataValue1").textContent = data.name;
+        document.querySelector("#dataValue2").textContent = data.hair_color;
+    }
+    else if (queryType == "planets")
+    {
+        document.querySelector("#dataLabel1").textContent = "Name: ";
+        document.querySelector("#dataLabel2").textContent = "Diameter: "
+        document.querySelector("#dataValue1").textContent = data.name;
+        document.querySelector("#dataValue2").textContent = data.diameter;
+    }
+    else if (queryType == "starships")
+    {
+        document.querySelector("#dataLabel1").textContent = "Name: ";
+        document.querySelector("#dataLabel2").textContent = "Cost in Credits: "
+        document.querySelector("#dataValue1").textContent = data.name;
+        document.querySelector("#dataValue2").textContent = data.cost_in_credits;
+    }
 }
 
 // create a new function called 'updateInfo()' that receives the data from 

3f36f644e225ebc7b7c9cad5eafadf1615fed5cc
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-10-18 15:13:40 -0500

    Complete 'Scavenger Hunt' assignment

--- week4/assignments/scavenger/scavenger.js
+++ week4/assignments/scavenger/scavenger.js
@@ -1,10 +1,18 @@
 // Create an event listenter for the 'submit' button that calls the combineLists() function
+document.querySelector("#submit").addEventListener("click", combineLists);
 
 // Modify the combineLists() function to create arrays from each scavenger hunt element in the scavenger.html document. 
 // Then use the spread operator to combine these arrays into a single array. 
 // Sort that array and display it in the 'AllItems' element on the scavenger.html document.
 
 function combineLists() {
+    document.querySelector("#AllItems").textContent = "";
+    const arr1 = [document.querySelector("#scavenger-0").value.split(",")];
+    const arr2 = [document.querySelector("#scavenger-1").value.split(",")];
+    const arr3 = [document.querySelector("#scavenger-2").value.split(",")];
+    const arr4 = [document.querySelector("#scavenger-3").value.split(",")];
+    const fullArr = [...arr1, ...arr2, ...arr3, ...arr4];
+    fullArr.sort().forEach((e, i) => (document.querySelector("#AllItems").textContent += `${fullArr[i]}, `));
 }
 
 // The following code is a bonus addition that facilitates data entry on the scavenger.html document.

2e3d125ef9ef1967066a1e618ba346cdc6d49b42
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-10-26 19:23:15 -0500

    Complete 'Modules and Classes' assignment

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,14 +1,42 @@
 // Declare any necessary variables.
-
-// Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
+let score;
+let maxScore;
+// Add an evemt listener that responds to the click of the "print" button by calling a function to instantiate
 //  a new student object, and another function to print the certificate.
+document.querySelector("#print").addEventListener("click", newStudent)
 
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
+document.querySelector("#reset").addEventListener("click", () => {
+    let formElements = [...document.querySelectorAll("input")];
+    let certElements = [...document.querySelectorAll(".cert-data")]
+
+    formElements.forEach((e, i) => {
+        formElements[i].value = "";
+    })
+
+    certElements.forEach((e, i) => {
+        certElements[i].textContent = "";
+    })
+})
 
 // Create a function that instantiates a new student object with the input from the HTML form.
+function newStudent() {
+    let currentStudent = new Student();
+    printCert(currentStudent);
+}
 
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
+function printCert(cs) {
+    document.querySelector("#certStudentName").textContent = cs.name;
+    document.querySelector("#certClassName").textContent = cs.course;
+    cs.sumScores();
+    cs.possScores();
+    cs.ltrGrade(score, maxScore);
+}
 
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
+function convertToNum(arr) {
+    arr.forEach((e, i) => (arr[i] = Number(arr[i])));
+}
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -3,3 +3,51 @@
 // - a method that adds up all the student's scores
 // - a method that adds up all the possible scores
 // - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+class Student {
+    constructor() {
+        this.name = document.querySelector("#studentName").value;
+        this.course = document.querySelector("#className").value;
+        this.scores = document.querySelector("#studentScores").value.split(",");
+        this.maxScores = document.querySelector("#possibleScores").value.split(",");
+    }
+
+    sumScores() {
+        convertToNum(this.scores);
+        score = this.scores.reduce((p, c) => (p + c));
+    }
+
+    possScores() {
+        convertToNum(this.maxScores);
+        maxScore = this.maxScores.reduce((p, c) => (p + c));
+    }
+
+    ltrGrade(stuScore, topScore) {
+        if (((stuScore / topScore) * 100) >= 100) {
+            document.querySelector("#certGrade").textContent = "A+";
+        }
+        else if (((stuScore / topScore) * 100) < 100 && ((stuScore / topScore) * 100) >= 95) {
+            document.querySelector("#certGrade").textContent = "A-";
+        }
+        else if (((stuScore / topScore) * 100) < 95 && ((stuScore / topScore) * 100) >= 90) {
+            document.querySelector("#certGrade").textContent = "B+";
+        }
+        else if (((stuScore / topScore) * 100) < 90 && ((stuScore / topScore) * 100) >= 85) {
+            document.querySelector("#certGrade").textContent = "B-";
+        }
+        else if (((stuScore / topScore) * 100) < 85 && ((stuScore / topScore) * 100) >= 80) {
+            document.querySelector("#certGrade").textContent = "C+";
+        }
+        else if (((stuScore / topScore) * 100) < 80 && ((stuScore / topScore) * 100) >= 75) {
+            document.querySelector("#certGrade").textContent = "C-";
+        }
+        else if (((stuScore / topScore) * 100) < 75 && ((stuScore / topScore) * 100) >= 70) {
+            document.querySelector("#certGrade").textContent = "D+";
+        }
+        else if (((stuScore / topScore) * 100) < 70 && ((stuScore / topScore) * 100) >= 65) {
+            document.querySelector("#certGrade").textContent = "D-";
+        }
+        else if (((stuScore / topScore) * 100) < 65) {
+            document.querySelector("#certGrade").textContent = "F";
+        }
+    }
+}

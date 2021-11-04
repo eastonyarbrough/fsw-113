41fb08f5f1e83e3a7831bf3acec00e845ff6b95b
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-11-04 17:38:23 -0500

    Complete 'Weather App from API' assignment

--- week6/assignment/scripts/convertTemp.js
+++ week6/assignment/scripts/convertTemp.js
@@ -1,5 +1,8 @@
 // This script should return the results of a function that converts the temperature provided by the api (Kelvin).
 // Depending on the location of the city, the function should return degrees in either fahrenheit or centigrade.
-export default function convertKelvin(kelv) {
-    return ((parseFloat(kelv).toFixed(2) - 273.15) * (9 / 5) + 32).toFixed(2)
+export default function convertKelvin(kelv, country) {
+    if (country == "US")
+        return `${((parseFloat(kelv).toFixed(2) - 273.15) * (9 / 5) + 32).toFixed(2)} °F`
+    else
+        return `${(parseFloat(kelv).toFixed(2) - 273.15).toFixed(2)} °C`
 }
--- week6/assignment/scripts/weather.js
+++ week6/assignment/scripts/weather.js
@@ -6,39 +6,51 @@ import convertKelvin from "./convertTemp.js";
 let data;
 
 // create an event listener for the click of the goBttn that calls a function to fetch the weather data
-document.querySelector("#goBttn").addEventListener("click", async() => {
-    let cityQuery = document.querySelector("#city").value;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityQuery}&appid=0cbd48f13316e2c77e35baa8bdb5a0bf`)
-    data = await response.json();
-    apiQuery(data);
+document.querySelector("#goBttn").addEventListener("click", () => {
+    runRequest();
 })
+
+function runRequest() {
+    getData()
+        .then((data) => {
+            apiQuery(data);
+        })
+        .catch(err => console.error(err));
+}
 
 // create a function that calls the function that queries the api 
 // and then creates promises that will call a function to write the weather data to the HTML page        
-function apiQuery(info){
-    //Store and pass data
-    let dataTemp = `${convertKelvin(info.main.temp)} °F`;
-    let dataHumid = `${info.main.humidity}%`;
-    let dataCond = info.weather[0].main;
-    let dataCurTime = (new Date((info.dt - 18000) * 1000).toUTCString().split(" "))[4].split(":").join("");
-    let dataSunTime = (new Date((info.sys.sunset - 18000) * 1000).toUTCString().split(" "))[4].split(":").join("");
-    displayInfo(dataTemp, dataHumid, dataCond, dataCurTime, dataSunTime);
+async function getData() {
+    let cityQuery = document.querySelector("#city").value;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityQuery}&appid=0cbd48f13316e2c77e35baa8bdb5a0bf`)
+    data = await response.json();
+    return data;
 }
 
 // use an asynchronous call that fetches the current weather for the specified city, awaits it, 
 // and returns the resulting data
+function apiQuery(info){
+    //Store and pass data
+    let dataTemp = convertKelvin(info.main.temp, info.sys.country);
+    let dataHumid = `${info.main.humidity}%`;
+    let dataCond = info.weather[0].main;
+    let dataCurTime = (new Date((info.dt - 18000) * 1000).toUTCString().split(" "))[4].split(":").join("");
+    let dataSunRiseTime = (new Date((info.sys.sunset - 18000) * 1000).toUTCString().split(" "))[4].split(":").join("");
+    let dataSunSetTime = (new Date((info.sys.sunrise - 18000) * 1000).toUTCString().split(" "))[4].split(":").join("");
+    displayInfo(dataTemp, dataHumid, dataCond, dataCurTime, dataSunRiseTime, dataSunSetTime);
+}
 
 // create a function that writes the temperature (using local units), humidity, and conditions
 // this function should also change the background color of the weather app to blue during the daylight
 // hours at the specified city
-function displayInfo(temper, humid, cond, curTime, sunTime) {
+function displayInfo(temper, humid, cond, curTime, sunRiseTime, sunSetTime) {
     //Write weather info in DOM
     document.querySelector("#tempData").textContent = temper;
     document.querySelector("#humidData").textContent = humid;
     document.querySelector("#conditionsData").textContent = cond;
 
     //Change background if after sunset.
-    if (curTime >= sunTime) {
+    if (curTime >= sunRiseTime && curTime <= sunSetTime) {
         document.querySelector(".weatherWrapper").setAttribute("style", `background-color: ${changeColor(true)}; color: white; border-radius: 8px;`);
     }
     else {
